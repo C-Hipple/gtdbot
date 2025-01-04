cf123e12aee622cc2a4f41d7eb0d8b001ce2b5a6
Chris <hipplec@Chriss-MacBook-Pro.local>
2025-01-03 20:51:54 -0500

    elisp helper upgrades

--- gtdbot.el
+++ gtdbot.el
@@ -1,15 +1,25 @@
+;;;###autoload
+(defun run-gtdbot-oneoff ()
+  "Runs gtdbot with the oneoff flag to update reviews.org"
+  (interactive)
+  (shell-command "gtdbot --oneoff" "*gtdbot*"))
+
+(define-key evil-normal-state-map (kbd ", r l") 'run-gtdbot-oneoff) ;; l for list?
+
+;; Theese are testing helper functions to make development a little bit easier
+;;;###autoload
+(defun run-gtdbot-parse-test()
+  "Runs gtdbot with the parse flag to check parsing reviews.org"
+  (interactive)
+  (async-shell-command "gtdbot --parse" "*gtdbot*"))
+
+(define-key evil-normal-state-map (kbd ", r p") 'run-gtdbot-parse-test)
+
+
+;;;###autoload
 (defun reset-reviews-org ()
   (interactive)
   (async-shell-command "cp ~/gtdbot/reviews_template.org ~/gtd/reviews.org")
   )
 
 (define-key evil-normal-state-map (kbd ", r b") 'reset-reviews-org)
-
-
-(defun run-gtdbot-parse-test()
-  "Runs gtdbot with the parse flag to check parsing reviews.org"
-  (interactive)
-  (async-shell-command "gtdbot --parse" "*gtdbot*"))
-
-
-(define-key evil-normal-state-map (kbd ", r p") 'run-gtdbot-parse-test)
