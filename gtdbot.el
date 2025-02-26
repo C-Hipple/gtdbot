96fe94ab1d54a0dd3f5abb82f32e5de66e234204
Chris Hipple <chrishipple@multimediallc.com>
2025-02-26 11:17:33 -0500

    improve reset command

--- gtdbot.el
+++ gtdbot.el
@@ -29,8 +29,7 @@
 ;;;###autoload
 (defun reset-reviews-org ()
   (interactive)
-  (async-shell-command "cp ~/gtdbot/reviews_template.org ~/gtd/reviews.org")
-  )
+  (shell-command "cp ~/gtdbot/reviews_template.org ~/gtd/reviews.org"))
 
 (define-key evil-normal-state-map (kbd ", r b") 'reset-reviews-org)
 
