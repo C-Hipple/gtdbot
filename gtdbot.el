118111636f1f76f2a52e61a340b8e6e49b23a841
chris <chris.hipple@gmail.com>
2025-05-10 22:18:20 -0400

    add org-review to this package

--- gtdbot.el
+++ gtdbot.el
@@ -1,18 +1,40 @@
-(require 'magit-delta)
+;;; gtdbot.el --- Tools for implementing Getting Things Done methodology in emacs -*- lexical-binding: t; -*-
 
+;; Author: Chris Hipple
+;; URL: https://github.com/C-Hipple/gtdbot
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "25.1"))
+
+;; SPDX-License-Identifier: GPL-3.0+
+
+;;; Commentary:
+
+;; This package provides tooling for running gtdbot in (defined in the same repo, built with go) and for reviewing closed / archived tasks
+;;
+;; Usage:
+
+;; For running gtbot, simply call run-gtdbot-oneoff or run-gtdbot-service depending on what you want.  Service runs the sync every minute (configurable via toml)
+
+;; For using the review functionality, open one of your files called out in org-agenda list and then call (org-agenda).  There you will have a new binding of R to enter your review periods.
+
+;;; Code:
 ;;;###autoload
 (defun delta-wash()
-  "interactive of the call magit-delta function"
+  "interactive of the call magit-delta function if you have the package installed."
   (interactive)
-  (magit-delta-call-delta-and-convert-ansi-escape-sequences))
+  (if (functionp magit-delta-call-delta-and-convert-ansi-escape-sequences)
+      (magit-delta-call-delta-and-convert-ansi-escape-sequences)
+    (message "You do not have magit-delta installed!")))
 
-(define-key evil-normal-state-map (kbd ", r d") 'delta-wash) ;; d for delta?
 
 
 (defun gtdbot--callback (x)
+  ;; TODO: Allow for setting the reviews file
   (with-current-buffer (find-file-noselect "~/gtd/reviews.org")
-    (magit-delta-call-delta-and-convert-ansi-escape-sequences)
-    (save-buffer))
+    (if (functionp magit-delta-call-delta-and-convert-ansi-escape-sequences)
+        (progn
+          (magit-delta-call-delta-and-convert-ansi-escape-sequences)
+          (save-buffer))))
   (message "gtdbot sync complete!"))
 
 ;;;###autoload
@@ -22,16 +44,18 @@
   (async-start-process "gtdbot-async" "gtdbot" 'gtdbot--callback "--oneoff"))
 
 
-(define-key evil-normal-state-map (kbd ", r l") 'run-gtdbot-oneoff) ;; l for list?
-
-
 ;;;###autoload
 (defun run-gtdbot-service ()
   "Runs gtdbot with the oneoff flag to update reviews.org"
   (interactive)
   (async-shell-command "gtdbot" "*gtdbot*"))
 
-(define-key evil-normal-state-map (kbd ", r S") 'run-gtdbot-service) ;; s I already have bound to review start at url
+;; I only bind for evil-mode
+(if (not (eq evil-normal-state-map nil))
+    (progn
+      (define-key evil-normal-state-map (kbd ", r S") 'run-gtdbot-service) ;; s I already have bound to review start at url
+      (define-key evil-normal-state-map (kbd ", r l") 'run-gtdbot-oneoff) ;; l for list?
+      (define-key evil-normal-state-map (kbd ", r d") 'delta-wash)))
 
 
 ;; Theese are testing helper functions to make development a little bit easier
@@ -43,14 +67,86 @@
 
 (define-key evil-normal-state-map (kbd ", r p") 'run-gtdbot-parse-test)
 
+;; Below this point is the code for doing org agenda reviews in emacs
+;; To be honest, I copied this from a blog which seems to have been since taken down and I can't find it.
 
-;;;###autoload
-(defun reset-reviews-org ()
-  (interactive)
-  (shell-command "cp ~/gtdbot/reviews_template.org ~/gtd/reviews.org"))
-;; (shell-command "cp ~/reviews_template_testdata.org ~/gtd/reviews.org"))
+(setq gtdbot-org-agenda-files '("~/gtd/inbox.org" "~/gtd/gtd.org" "~/gtd/notes.org" "~/gtd/next_actions.org" "~/gtd/reviews.org"))
 
-(define-key evil-normal-state-map (kbd ", r b") 'reset-reviews-org)
+;; define "R" as the prefix key for reviewing what happened in various time periods
+(setq org-agenda-custom-commands
+      '(("c" "Work" tags-todo "work" ;; (1) (2) (3) (4)
+         ((org-agenda-files gtdbot-org-agenda-files) ) ;; (5)
+         (org-agenda-sorting-strategy '(priority-up effort-down))) ;; (5) cont.
+        ("~/computer.html"))) ;; (6)
 
-;; probably don't need this often
-(define-key evil-normal-state-map (kbd "SPC g B") (lambda () (interactive) (switch-to-buffer "*gtdbot*" nil t)))
+(add-to-list 'org-agenda-custom-commands
+             '("R" . "Review" ))
+
+;; Common settings for all reviews
+(setq efs/org-agenda-review-settings
+      '((org-agenda-files gtdbot-org-agenda-files)
+        (org-agenda-show-all-dates t)
+        (org-agenda-start-with-log-mode t)
+        (org-agenda-start-with-clockreport-mode t)
+        (org-agenda-archives-mode t)
+        ;; I don't care if an entry was archived
+        (org-agenda-hide-tags-regexp
+         (concat org-agenda-hide-tags-regexp
+                 "\\|ARCHIVE"))
+        ))
+;; Show the agenda with the log turn on, the clock table show and
+;; archived entries shown.  These commands are all the same exept for
+;; the time period.
+(add-to-list 'org-agenda-custom-commands
+             `("Rw" "Week in review"
+               agenda ""
+               ;; agenda settings
+               ,(append
+                 efs/org-agenda-review-settings
+                 '((org-agenda-span 'week)
+                   (org-agenda-start-on-weekday 0)
+                   (org-agenda-overriding-header "Week in Review"))
+                 )
+               ("~//review/week.html")
+               ))
+
+
+(add-to-list 'org-agenda-custom-commands
+             `("Rd" "Day in review"
+               agenda ""
+               ;; agenda settings
+               ,(append
+                 efs/org-agenda-review-settings
+                 '((org-agenda-span 'day)
+                   (org-agenda-overriding-header "Day in Review"))
+                 )
+               ("~//review/day.html")
+               ))
+
+(add-to-list 'org-agenda-custom-commands
+             `("Rm" "Month in review"
+               agenda ""
+               ;; agenda settings
+               ,(append
+                 efs/org-agenda-review-settings
+                 '((org-agenda-span 'month)
+                   (org-agenda-start-day "01")
+                   (org-read-date-prefer-future nil)
+                   (org-agenda-overriding-header "Month in Review"))
+                 )
+               ("~//review/month.html")
+               ))
+
+(add-to-list 'org-agenda-custom-commands
+             `("Rs" "Sprint in review"
+               agenda ""
+               ;; agenda settings
+               ,(append
+                 efs/org-agenda-review-settings
+                 '((org-agenda-span 10)
+                   (org-agenda-start-day "-6d")
+                   (org-agenda-start-on-weekday nil)
+                   (org-agenda-overriding-header "Sprint Review"))
+                 )
+               ("~//review/sprint.html")
+               ))
